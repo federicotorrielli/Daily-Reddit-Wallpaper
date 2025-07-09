6b11b9f520eeb4f271467c98883f9f86d6433f3d
Federico Torrielli <evilscript@protonmail.com>
2025-07-09 18:30:19 +0200

    refactor: streamline configuration loading and enhance argument parsing in change_wallpaper_reddit.py

--- change_wallpaper_reddit.py
+++ change_wallpaper_reddit.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import ctypes
 import json
 import os
 import platform
@@ -10,64 +9,72 @@ import random
 import re
 import subprocess
 import sys
-from collections import defaultdict
 from configparser import ConfigParser
 from pathlib import Path
 
-import praw
-import requests
+import praw  # type: ignore
+import requests  # type: ignore
+
+# Platform detection constants
+PLATFORM = platform.system()
+IS_LINUX = PLATFORM == "Linux"
+IS_WINDOWS = PLATFORM == "Windows"
+IS_DARWIN = PLATFORM == "Darwin"
+
+# Default configuration
+DEFAULT_CONFIG = {
+    "subreddit": "wallpaper",
+    "nsfw": False,
+    "time": "all",
+    "display": "0",
+    "output": "Wallpapers",
+    "sort": "hot",
+    "limit": 20,
+    "random": False,
+    "flair": "",
+}
+
+# URL regex pattern (compiled once for performance)
+URL_STRIP_PATTERN = re.compile(r"\?.*")
 
 
 def load_config():
-    default = defaultdict(str)
-    default["subreddit"] = "wallpaper"
-    default["nsfw"] = False
-    default["time"] = "all"
-    default["display"] = "0"
-    default["output"] = "Wallpapers"
-    default["sort"] = "hot"
-    default["limit"] = 20
-    default["random"] = False
-    default["flair"] = ""
+    """Load configuration from file or return defaults"""
+    config_path = (
+        Path.home() / ".config" / "change_wallpaper_reddit.rc"
+        if IS_LINUX
+        else Path(sys.argv[0]).parent / "change_wallpaper_reddit.rc"
+    )
 
-    # If Linux, use config_path_linux otherwise use config_path_windows
-    config_path_linux = Path.home() / ".config" / "change_wallpaper_reddit.rc"
-    config_path_windows = Path(sys.argv[0]).parent / "change_wallpaper_reddit.rc"
-
-    if platform.system() == "Linux":
-        config_path = config_path_linux
-    else:
-        config_path = config_path_windows
-    section_name = "root"
     try:
-        conf = ConfigParser(default)
+        conf = ConfigParser(DEFAULT_CONFIG)
         with open(config_path, "r") as stream:
-            config_string = f"[{section_name}]\n{stream.read()}"
+            config_string = f"[root]\n{stream.read()}"
             conf.read_string(config_string)
 
-            ret = {}
+            # Build config dict with error handling
+            config = {}
+            config_mappings = [
+                ("subreddit", conf.get),
+                ("nsfw", conf.getboolean),
+                ("display", conf.getint),
+                ("time", conf.get),
+                ("output", conf.get),
+                ("sort", conf.get),
+                ("limit", conf.getint),
+                ("random", conf.getboolean),
+                ("flair", conf.get),
+            ]
 
-            # Add a value to ret, printing an error message if there is an error
-            def add_to_ret(fun, name):
+            for key, getter in config_mappings:
                 try:
-                    ret[name] = fun(section_name, name)
-                except ValueError:
-                    ret[name] = default[name]
+                    config[key] = getter("root", key)
+                except (ValueError, TypeError):
+                    config[key] = DEFAULT_CONFIG[key]
 
-            add_to_ret(conf.get, "subreddit")
-            add_to_ret(conf.getboolean, "nsfw")
-            add_to_ret(conf.getint, "display")
-            add_to_ret(conf.get, "time")
-            add_to_ret(conf.get, "output")
-            add_to_ret(conf.get, "sort")
-            add_to_ret(conf.get, "limit")
-            add_to_ret(conf.getboolean, "random")
-            add_to_ret(conf.get, "flair")
-
-            return ret
-
+            return config
     except IOError:
-        return default
+        return DEFAULT_CONFIG.copy()
 
 
 config = load_config()
@@ -122,7 +129,7 @@ def parse_args():
     parser.add_argument(
         "-l",
         "--limit",
-        type=str,
+        type=int,
         default=config["limit"],
         help="Set a limit to pull posts",
     )
@@ -149,215 +156,263 @@ def parse_args():
     return arguments
 
 
-def get_top_image(sub_reddit):
+def get_top_image(sub_reddit, args):
     """Get image link of most upvoted wallpaper of the day
     :sub_reddit: name of the sub reddit
+    :args: parsed arguments
     :return: the image link
     """
+    # Use generator dispatch for better performance
     if args.sort == "top":
-        submissions = sub_reddit.top(time_filter=args.time, limit=int(args.limit))
+        submissions = sub_reddit.top(time_filter=args.time, limit=args.limit)
     elif args.sort == "new":
-        submissions = sub_reddit.new(limit=int(args.limit))
+        submissions = sub_reddit.new(limit=args.limit)
     else:
-        submissions = sub_reddit.hot(limit=int(args.limit))
+        submissions = sub_reddit.hot(limit=args.limit)
 
     if args.random:
         submissions = sorted(submissions, key=lambda k: random.random())
 
     for submission in submissions:
-        ret = {"id": submission.id}
-        ret["subreddit"] = submission.subreddit.display_name
-        print(ret["subreddit"])
         if not args.nsfw and submission.over_18:
             continue
+
         # Filter by flair if specified
         if args.flair and (
             not submission.link_flair_text
             or args.flair.lower() not in submission.link_flair_text.lower()
         ):
             continue
+
         url = submission.url
-        print(f"url : {url}")
         # Strip trailing arguments (after a '?')
-        url = re.sub(r"\?.*", "", url)
-        ret["type"] = url.split(".")[-1]
+        url = URL_STRIP_PATTERN.sub("", url)
+
+        # Extract file extension
+        file_extension = url.split(".")[-1].lower()
 
         if url.endswith((".jpg", ".png", ".jpeg")):
-            ret["url"] = url
+            return {
+                "id": submission.id,
+                "subreddit": submission.subreddit.display_name,
+                "url": url,
+                "type": file_extension,
+            }
         # Imgur support
         elif "imgur.com" in url and "/a/" not in url and "/gallery/" not in url:
             if url.endswith("/new"):
                 url = url.rsplit("/", 1)[0]
             id_toget = url.rsplit("/", 1)[1].rsplit(".", 1)[0]
-            ret["url"] = f"http://i.imgur.com/{id_toget}.jpg"
-        else:
-            continue
+            return {
+                "id": submission.id,
+                "subreddit": submission.subreddit.display_name,
+                "url": f"http://i.imgur.com/{id_toget}.jpg",
+                "type": "jpg",
+            }
 
-        return ret
+    return None
 
 
 def detect_desktop_environment():
     """Get current Desktop Environment
-       http://stackoverflow.com
-       /questions/2035657/what-is-my-current-desktop-environment
-    :return: environment
+    :return: environment dict or None
     """
-    environment = {}
+    if not IS_LINUX:
+        return None
+
+    # Environment variables for detection
     d_session = os.environ.get("DESKTOP_SESSION")
     xdg_current_desktop = os.environ.get("XDG_CURRENT_DESKTOP")
+
+    # KDE detection
     if os.environ.get("KDE_FULL_SESSION") == "true":
-        environment["name"] = "kde"
-        environment["command"] = """
-                    qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
-                        var allDesktops = desktops();
-                        print (allDesktops);
-                        for (i=0;i<allDesktops.length;i++) {{
-                            d = allDesktops[i];
-                            d.wallpaperPlugin = "org.kde.image";
-                            d.currentConfigGroup = Array("Wallpaper",
-                                                   "org.kde.image",
-                                                   "General");
-                            d.writeConfig("Image", "file:///{save_location}")
-                        }}
-                    '
-                """
+        return {
+            "name": "kde",
+            "command": (
+                "qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '"
+                "var allDesktops = desktops();"
+                "for (i=0;i<allDesktops.length;i++) {"
+                "    d = allDesktops[i];"
+                '    d.wallpaperPlugin = "org.kde.image";'
+                '    d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");'
+                '    d.writeConfig("Image", "file:///{save_location}");'
+                "}"
+                "'"
+            ),
+        }
+
+    # GNOME detection
     elif os.environ.get("GNOME_DESKTOP_SESSION_ID") or (
         xdg_current_desktop and "GNOME" in xdg_current_desktop.upper()
     ):
-        environment["name"] = "gnome"
-        environment["command"] = (
-            "gsettings set org.gnome.desktop.background picture-uri file://{save_location} && gsettings set org.gnome.desktop.background picture-uri-dark file://{save_location}"
-        )
-    elif d_session == "Lubuntu":
-        environment["name"] = "lubuntu"
-        environment["command"] = "pcmanfm -w {save_location} --wallpaper-mode=fit"
-    elif d_session == "mate":
-        environment["name"] = "mate"
-        environment["command"] = (
-            "gsettings set org.mate.background picture-filename {save_location}"
-        )
-    elif d_session in ("i3", "leftwm", "dwm"):
-        environment["name"] = "i3"
-        environment["command"] = "feh --bg-scale {save_location}"
-    else:
+        return {
+            "name": "gnome",
+            "command": (
+                "gsettings set org.gnome.desktop.background picture-uri file://{save_location} && "
+                "gsettings set org.gnome.desktop.background picture-uri-dark file://{save_location}"
+            ),
+        }
+
+    # Other desktop environments
+    desktop_configs = {
+        "Lubuntu": {
+            "name": "lubuntu",
+            "command": "pcmanfm -w {save_location} --wallpaper-mode=fit",
+        },
+        "mate": {
+            "name": "mate",
+            "command": "gsettings set org.mate.background picture-filename {save_location}",
+        },
+    }
+
+    if d_session in desktop_configs:
+        return desktop_configs[d_session]
+
+    # Window manager detection
+    if d_session in ("i3", "leftwm", "dwm"):
+        return {"name": "i3", "command": "feh --bg-scale {save_location}"}
+
+    # XFCE detection
+    try:
+        info = subprocess.getoutput("xprop -root _DT_SAVE_MODE")
+        if ' = "xfce4"' in info:
+            return {
+                "name": "xfce",
+                "command": (
+                    "xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image "
+                    "-s {save_location}"
+                ),
+            }
+    except (OSError, RuntimeError):
+        pass
+
+    return None
+
+
+def set_wallpaper(save_location, args):
+    """Set wallpaper based on the operating system"""
+    if IS_LINUX:
+        desktop_environment = detect_desktop_environment()
+        if desktop_environment:
+            os.system(
+                desktop_environment["command"].format(save_location=save_location)
+            )
+        else:
+            print("Unsupported desktop environment")
+
+    elif IS_WINDOWS:
         try:
-            info = subprocess.getoutput("xprop -root _DT_SAVE_MODE")
-            if ' = "xfce4"' in info:
-                environment["name"] = "xfce"
-                environment["command"] = (
-                    "xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s {save_location}"
-                )
-        except (OSError, RuntimeError):
-            environment = None
-            pass
-    return environment
+            import ctypes
+
+            if hasattr(ctypes, "windll"):
+                ctypes.windll.user32.SystemParametersInfoW(20, 0, str(save_location), 3)
+            else:
+                print("Windows wallpaper setting not available")
+        except (ImportError, AttributeError):
+            print("Windows wallpaper setting not available")
+
+    elif IS_DARWIN:
+        if args.display == 0:
+            command = (
+                'osascript -e \'tell application "System Events"'
+                "set desktopCount to count of desktops"
+                "repeat with desktopNumber from 1 to desktopCount"
+                "    tell desktop desktopNumber"
+                '        set picture to "{save_location}"'
+                "    end tell"
+                "end repeat"
+                "end tell'"
+            ).format(save_location=save_location)
+        else:
+            command = (
+                'osascript -e \'tell application "System Events"'
+                "set desktopCount to count of desktops"
+                "tell desktop {display}"
+                '    set picture to "{save_location}"'
+                "end tell"
+                "end tell'"
+            ).format(display=args.display, save_location=save_location)
+        os.system(command)
 
 
-if __name__ == "__main__":
+def load_credentials(client_id, api_key):
+    """Load credentials from file or command line"""
+    if client_id and api_key:
+        return client_id, api_key
+
+    # Load from file if not provided
+    credentials_path = Path(sys.argv[0]).parent / "credentials.json"
+    try:
+        with open(credentials_path) as f:
+            params = json.load(f)
+        return (client_id or params.get("client_id"), api_key or params.get("api_key"))
+    except (IOError, json.JSONDecodeError):
+        return client_id, api_key
+
+
+def download_image(url, save_location):
+    """Download image with better error handling"""
+    try:
+        response = requests.get(url, allow_redirects=False, timeout=30)
+        response.raise_for_status()
+
+        if save_location.is_file():
+            return True
+
+        # Create directories if they don't exist
+        save_location.parent.mkdir(parents=True, exist_ok=True)
+
+        # Write to disk
+        with open(save_location, "wb") as fo:
+            for chunk in response.iter_content(4096):
+                fo.write(chunk)
+
+        return True
+    except requests.RequestException as e:
+        print(f"Error downloading image: {e}")
+        return False
+
+
+def main():
     args = parse_args()
     subreddit = args.subreddit
     save_dir = args.output
 
-    supported_linux_desktop_envs = ["gnome", "mate", "kde", "lubuntu", "i3", "xfce"]
+    # Load credentials
+    client_id, api_key = load_credentials(args.client_id, args.api_key)
 
-    client_id = args.client_id
-    api_key = args.api_key
-
-    if not all([client_id, api_key]):
-        # Load credentials from credentials.json file
-        credentials_path = Path(sys.argv[0]).parent / "credentials.json"
-        try:
-            with open(credentials_path) as f:
-                params = json.load(f)
-            client_id = client_id or params.get("client_id")
-            api_key = api_key or params.get("api_key")
-        except (IOError, json.JSONDecodeError):
-            pass
-
-    if not all([client_id, api_key]):
+    if not (client_id and api_key):
         sys.exit(
             "Error: client_id and api_key not provided. "
             "Please provide them as arguments (--client-id, --api-key) "
             "or in credentials.json."
         )
 
-    # Python Reddit Api Wrapper
-    r = praw.Reddit(
-        client_id=client_id,
-        client_secret=api_key,
-        user_agent=f"Get top wallpaper from /r/{subreddit}",
-    )
+    # Initialize Reddit API
+    try:
+        r = praw.Reddit(
+            client_id=client_id,
+            client_secret=api_key,
+            user_agent=f"Get top wallpaper from /r/{subreddit}",
+        )
+    except Exception as e:
+        sys.exit(f"Error initializing Reddit API: {e}")
 
     # Get top image link
-    image = get_top_image(r.subreddit(subreddit))
-    try:
-        if "url" not in image:
-            sys.exit(
-                "Error: No suitable images were found, the program is now exiting."
-            )
-    except TypeError:
+    image = get_top_image(r.subreddit(subreddit), args)
+    if not image:
         sys.exit("Error: No suitable images were found, the program is now exiting.")
 
-    # Request image
-    response = requests.get(image["url"], allow_redirects=False)
-    # If image is available, proceed to save
-    if response.status_code == requests.codes.ok:
-        # Get home directory and location where image will be saved
-        # (default location for Ubuntu is used)
-        save_location = (
-            Path.home() / save_dir / f"{subreddit}-{image['id']}.{image['type']}"
-        )
+    # Download and set wallpaper
+    save_location = (
+        Path.home() / save_dir / f"{subreddit}-{image['id']}.{image['type']}"
+    )
 
-        if not save_location.is_file():
-            # Create folders if they don't exist
-            save_location.parent.mkdir(parents=True, exist_ok=True)
-
-            # Write to disk
-            with open(save_location, "wb") as fo:
-                for chunk in response.iter_content(4096):
-                    fo.write(chunk)
-
-        # Check OS and environments
-        platform_name = platform.system()
-        if platform_name.startswith("Lin"):
-            # Check desktop environments for linux
-            desktop_environment = detect_desktop_environment()
-            if (
-                desktop_environment
-                and desktop_environment["name"] in supported_linux_desktop_envs
-            ):
-                os.system(
-                    desktop_environment["command"].format(save_location=save_location)
-                )
-            else:
-                print("Unsupported desktop environment")
-
-        # Windows
-        if platform_name.startswith("Win"):
-            ctypes.windll.user32.SystemParametersInfoW(20, 0, str(save_location), 3)
-
-        # OS X/macOS
-        if platform_name.startswith("Darwin"):
-            if args.display == 0:
-                command = """
-                        osascript -e 'tell application "System Events"
-                            set desktopCount to count of desktops
-                            repeat with desktopNumber from 1 to desktopCount
-                                tell desktop desktopNumber
-                                    set picture to "{save_location}"
-                                end tell
-                            end repeat
-                        end tell'
-                          """.format(save_location=save_location)
-            else:
-                command = """osascript -e 'tell application "System Events"
-                                set desktopCount to count of desktops
-                                tell desktop {display}
-                                    set picture to "{save_location}"
-                                end tell
-                            end tell'""".format(
-                    display=args.display, save_location=save_location
-                )
-            os.system(command)
+    if download_image(image["url"], save_location):
+        set_wallpaper(save_location, args)
     else:
-        sys.exit("Error: Image url is not available, the program is now exiting.")
+        sys.exit("Error: Failed to download image.")
+
+
+if __name__ == "__main__":
+    main()
